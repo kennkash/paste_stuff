d3993982c81150899adcce59710e135d98903a94
kennkash <kkashmiry23@gmail.com>
2025-12-18 12:46:04 -0600

    Update fzf.sh

--- fzf.sh
+++ fzf.sh
@@ -22,3 +22,42 @@ export FZF_DEFAULT_OPTS="\
 --color=input-border:#6E738D,input-label:#CAD3F5 \
 --color=header-border:#6E738D,header-label:#CAD3F5 \
 "
+
+
+
+fo() {
+  local file
+  # We use git ls-files as the source, falling back to find if not in a git repo
+  file=$( (git ls-files 2>/dev/null || find . -maxdepth 3 -not -path '*/.*') | fzf --query="$1" --select-1 --exit-0 --preview 'bat --color=always {} 2>/dev/null || cat {}')
+
+  if [[ -n "$file" ]]; then
+    code-server "$file"
+  fi
+}
+
+
+
+# fhe - repeat history edit
+writecmd (){ perl -e 'ioctl STDOUT, 0x5412, $_ for split //, do{ chomp($_ = <>); $_ }' ; }
+
+fhe() {
+  ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -re 's/^\s*[0-9]+\s*//' | writecmd
+}
+
+
+fkill() {
+    local pid
+    if [ "$UID" != "0" ]; then
+        pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
+    else
+        pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+    fi
+
+    if [ "x$pid" != "x" ]
+    then
+        echo $pid | xargs kill -${1:-9}
+    fi
+}
+
+
+
